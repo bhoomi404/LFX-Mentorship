ce7ad6ed56eb73efec829bb2186f4f1bb9664dc4
bhoomi404 <bhoomikokate2005@gmail.com>
2025-07-30 03:30:15 +0530

    Update StackModule.scala

--- src/main/scala/stack/StackModule.scala
+++ src/main/scala/stack/StackModule.scala
@@ -3,58 +3,45 @@ package stack
 import chisel3._
 import chisel3.util._
 
-class StackModule(val dataWidth: Int, val len: Int) extends Module {
+class StackModule(val dataWidth: Int = 8, val len: Int = 32) extends Module {
   val io = IO(new Bundle {
-    val in         = Input(UInt(32.W))          // Instruction input
-    val out        = Output(UInt(dataWidth.W))  // Data output (for POP/PEEK)
-    val underflow  = Output(Bool())             // Underflow flag
-    val overflow   = Output(Bool())             // Overflow flag
-    val isEmpty    = Output(Bool())             // True if stack is empty
-    val isFull     = Output(Bool())             // True if stack is full
-    val popped     = Output(Bool())             // True if POP occurred
-    val peeked     = Output(Bool())             // True if PEEK occurred
+    val in         = Input(UInt(dataWidth.W))    // Data to push
+    val push       = Input(Bool())               // Push signal
+    val pop        = Input(Bool())               // Pop signal
+    val peek       = Input(Bool())               // Peek signal
+    val out        = Output(UInt(dataWidth.W))   // Output data
+    val underflow  = Output(Bool())              // Underflow flag
+    val overflow   = Output(Bool())              // Overflow flag
   })
 
-  val PUSH_OPCODE = "b0100111".U(7.W)
-  val POP_OPCODE  = "b1000011".U(7.W)
-  val PEEK_OPCODE = "b1000000".U(7.W)
-
-  val stack = Reg(Vec(len, UInt(dataWidth.W)))
+  val stackMem = Mem(len, UInt(dataWidth.W))
   val sp = RegInit(0.U(log2Ceil(len + 1).W))  // Stack pointer
 
-  val opcode = io.in(6, 0)
-  val imm    = io.in(31, 7)
-  val dataToPush = imm.zext().asUInt()(dataWidth - 1, 0)
+  val outReg = RegInit(0.U(dataWidth.W))
+  io.out := outReg
 
-  io.out        := 0.U
-  io.underflow  := false.B
-  io.overflow   := false.B
-  io.isEmpty    := sp === 0.U
-  io.isFull     := sp === len.U
-  io.popped     := false.B
-  io.peeked     := false.B
+  io.underflow := false.B
+  io.overflow := false.B
 
-  when(opcode === PUSH_OPCODE) {
-    when(io.isFull) {
+  when(io.push && !io.pop) {
+    when(sp === len.U) {
       io.overflow := true.B
     } .otherwise {
-      stack(sp) := dataToPush
+      stackMem(sp) := io.in
       sp := sp + 1.U
     }
-  } .elsewhen(opcode === POP_OPCODE) {
-    when(io.isEmpty) {
+  } .elsewhen(io.pop && !io.push) {
+    when(sp === 0.U) {
       io.underflow := true.B
     } .otherwise {
       sp := sp - 1.U
-      io.out := stack(sp - 1.U)
-      io.popped := true.B
+      outReg := stackMem(sp - 1.U)
     }
-  } .elsewhen(opcode === PEEK_OPCODE) {
-    when(io.isEmpty) {
+  } .elsewhen(io.peek) {
+    when(sp === 0.U) {
       io.underflow := true.B
     } .otherwise {
-      io.out := stack(sp - 1.U)
-      io.peeked := true.B
+      outReg := stackMem(sp - 1.U)
     }
   }
 }
