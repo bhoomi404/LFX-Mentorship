f3da41e5174501758e70a515fd82e7bbde8855d7
@Bhoomi-29 <bhoomikokate29@gmail.com>
2025-07-30 02:27:30 +0530

    Added StackModule implementation for LFX coding challenge

--- src/main/scala/stack/StackModule.scala
+++ src/main/scala/stack/StackModule.scala
@@ -1,23 +1,60 @@
 package stack
 
-import chisel3.stage.ChiselStage
-import java.nio.file.Paths
+import chisel3._
+import chisel3.util._
 
-// Your code starts here
+class StackModule(val dataWidth: Int, val len: Int) extends Module {
+  val io = IO(new Bundle {
+    val in         = Input(UInt(32.W))
+    val out        = Output(UInt(dataWidth.W))
+    val underflow  = Output(Bool())
+    val overflow   = Output(Bool())
+    val isEmpty    = Output(Bool())
+    val isFull     = Output(Bool())
+    val popped     = Output(Bool())
+    val peeked     = Output(Bool())
+  })
 
+  val PUSH_OPCODE = "b0100111".U(7.W)
+  val POP_OPCODE  = "b1000011".U(7.W)
+  val PEEK_OPCODE = "b1000000".U(7.W)
 
+  val stack = Reg(Vec(len, UInt(dataWidth.W)))
+  val sp = RegInit(0.U(log2Ceil(len + 1).W))
 
-// Your code ends here
+  val opcode = io.in(6, 0)
+  val imm    = io.in(31, 7)
+  val dataToPush = imm.zext().asUInt()(dataWidth - 1, 0)
 
-object SVGen extends App {
-  val out = Paths.get(
-    "out",
-    this.getClass
-      .getName
-      .stripSuffix("$")
-  ).toString
-  new ChiselStage().emitSystemVerilog(
-    new StackModule(args(0).toInt, args(1).toInt),
-    Array("--target-dir", out),
-  )
+  io.out := 0.U
+  io.underflow := false.B
+  io.overflow := false.B
+  io.isEmpty := sp === 0.U
+  io.isFull := sp === len.U
+  io.popped := false.B
+  io.peeked := false.B
+
+  when(opcode === PUSH_OPCODE) {
+    when(io.isFull) {
+      io.overflow := true.B
+    } .otherwise {
+      stack(sp) := dataToPush
+      sp := sp + 1.U
+    }
+  } .elsewhen(opcode === POP_OPCODE) {
+    when(io.isEmpty) {
+      io.underflow := true.B
+    } .otherwise {
+      sp := sp - 1.U
+      io.out := stack(sp - 1.U)
+      io.popped := true.B
+    }
+  } .elsewhen(opcode === PEEK_OPCODE) {
+    when(io.isEmpty) {
+      io.underflow := true.B
+    } .otherwise {
+      io.out := stack(sp - 1.U)
+      io.peeked := true.B
+    }
+  }
 }
